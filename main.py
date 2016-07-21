080ccb6e130f2c7d83cf465be3d7493f7dbe6b43
Jean <jeanbehrend@gmail.com>
2016-07-20 21:02:55 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+def filter_custom(l, f):
+    '''Filter a list using a function
+
+    Return a new list that contains all the elements e of l
+    for which f(e) is True
+
+    :param l: a list
+    :param f: a function that takes one argument and returns either
+        True or False
+    '''
+    pass
+
+def map_custom(l, f):
+    '''Map a list using a function
+
+    Return a new list that applies f(e) for every element e in l
+
+    :param l: a list
+    :param f: a function that takes one argument and returns a value'''
+
+    pass
+
+def reduce_custom(l, f, starting_value):
+    '''Reduce a list using a reducer function and a starting value
+
+    Return a single value that applies f(v, e) for every
+    e in l from left to right.  The initial value for v
+    should be starting_value, and subsequent values should be the previously
+    calculated value from f(v, e)
+
+    :param l: a list
+    :param f: a function that takes one argument and returns a value
+    :param starting_value: the beginning value for the reducer function
+        computation
+    '''
+    pass
+
